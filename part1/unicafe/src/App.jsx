30a6f28f526e9072b2cac22113719b0590e4961c
Martin Cappelletti Agustín <93543220+inkinvissible@users.noreply.github.com>
2024-12-02 17:22:18 +0000

    Todo funcionando, en progreso a la refactorización del código

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,24 +1,34 @@
 import { useState } from 'react'
 
-const Title = () => <h1>give feedback</h1>
+const Title = ({title}) => <h1>{title}</h1>
 
-const Statistics = ({good, neutral, bad}) => {
-  // let total = good + neutral + bad
-  // console.log("total ", total)
+const Button = () => {
 
-  // let average = (good + bad + neutral) / 3
-  // console.log("average", average)
+}
 
-  // let positive = (good / total) * 100
-  // console.log("positive", positive)
+const Statistics = ({ good, neutral, bad, all }) => {
+  let total = good + neutral + bad
+  let average = ((good - bad) / total)
+  let positive = (good / total) * 100
+
+  if (all.length === 0) {
+    return (
+      <div>
+        <Title title="statistics" />
+        <p>No Feedback given</p>
+      </div>
+    )
+  }
 
   return (
     <div>
-      <h2>statistics</h2>
+      <Title title="statistics" />
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
-
+      <p>All: {total}</p>
+      <p>Average: {average}</p>
+      <p>Positive: {positive} %</p>
 
     </div>
 
@@ -30,21 +40,34 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState([])
 
-  const handleGoodClick = () => setGood(good + 1)
-  const handleNeutralClick = () => setNeutral(neutral + 1)
-  const handleBadClick = () => setBad(bad + 1)
+  const handleGoodClick = () => {
+    setGood(good + 1)
+    setAll(all.concat('G'))
+  }
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+    setAll(all.concat('N'))
+  }
+  const handleBadClick = () => {
+    setBad(bad + 1)
+    setAll(all.concat('B'))
+  }
 
   return (
     <>
-    <Title />
-    <br />
-    <div>
-      <button onClick={handleGoodClick}>good</button>
-      <button onClick={handleNeutralClick}>neutral</button>
-      <button onClick={handleBadClick}>bad</button>
-    </div>
-    <Statistics good={good} neutral={neutral} bad={bad} />
+      <Title title="give feedback" />
+      <br />
+      <div>
+        <Button handleClick={handleGoodClick} text="good" />
+        <Button handleClick={handleNeutralClick} text="neutral" />
+        <Button handleClick={handleBadClick} text="bad" />
+        <button onClick={handleGoodClick}>good</button>
+        <button onClick={handleNeutralClick}>neutral</button>
+        <button onClick={handleBadClick}>bad</button>
+      </div>
+      <Statistics good={good} neutral={neutral} bad={bad} all={all} />
 
     </>
   )
