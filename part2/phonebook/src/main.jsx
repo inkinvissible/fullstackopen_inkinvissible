02c5a405ad649451eb8edb1d73ba6e4fd5bb2173
Martin Cappelletti Agustín <93543220+inkinvissible@users.noreply.github.com>
2024-12-04 18:02:02 +0000

    Json server

--- part2/phonebook/src/main.jsx
+++ part2/phonebook/src/main.jsx
@@ -1,6 +1,8 @@
 import ReactDOM from 'react-dom/client'
-
+import axios from 'axios'
 import App from './App'
+
+const promise = axios.get('')
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <App />
